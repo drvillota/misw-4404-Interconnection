d53b7c13fa69582011587ceff26b0882cef0049c
daaruiz_cib <daaruiz@bancolombia.com.co>
2024-11-24 22:29:31 -0500

    Refactor View

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -42,7 +42,7 @@ public class Controller<T> {
 			int option = lector.nextInt();
 			switch(option){
 			case 1:
-				view.printMessage("--------- \nCargar datos");
+				view.printObject("--------- \nCargar datos");
 				modelo = new Modelo(1); 
 				try 
 				{
@@ -51,59 +51,59 @@ public class Controller<T> {
 
 					e.printStackTrace();
 				}
-				view.printModelo(modelo);	
+				view.printObject(modelo);	
 
 				break;
 				
 			case 2:
-				view.printMessage("--------- \nIngrese el nombre del primer punto de conexión");
+				view.printObject("--------- \nIngrese el nombre del primer punto de conexión");
 				String punto1= lector.next();
 				lector.nextLine();
 				
-				view.printMessage("--------- \nIngrese el nombre del segundo punto de conexión");
+				view.printObject("--------- \nIngrese el nombre del segundo punto de conexión");
 				String punto2= lector.next();
 				lector.nextLine();
 				
 				String res1=modelo.req1String(punto1, punto2);
-				view.printMessage(res1);
+				view.printObject(res1);
 				
 				break;
 				
 			case 3:
 				String res2= modelo.req2String();
-				view.printMessage(res2);
+				view.printObject(res2);
 				break;
 				
 			case 4:
-				view.printMessage("--------- \nIngrese el nombre del primer país");
+				view.printObject("--------- \nIngrese el nombre del primer país");
 				String pais1= lector.next();
 				lector.nextLine();
 				
-				view.printMessage("--------- \nIngrese el nombre del segundo país");
+				view.printObject("--------- \nIngrese el nombre del segundo país");
 				String pais2= lector.next();
 				lector.nextLine();
 				
 				String res3= modelo.req3String(pais1, pais2);
-				view.printMessage(res3);
+				view.printObject(res3);
 				break;
 			case 5:
 				String res4= modelo.req4String();
-				view.printMessage(res4);
+				view.printObject(res4);
 				break;
 			case 6:
-				view.printMessage("--------- \nIngrese el nombre del punto de conexión");
+				view.printObject("--------- \nIngrese el nombre del punto de conexión");
 				String landing= lector.next();
 				lector.nextLine();
 				String res5= modelo.req5String(landing);
-				view.printMessage(res5);
+				view.printObject(res5);
 				break;
 			case 7:
-				view.printMessage("--------- \n Hasta pronto !! \n---------"); 
+				view.printObject("--------- \n Hasta pronto !! \n---------"); 
 				lector.close();
 				fin = true;
 				break;
 			default: 
-				view.printMessage("--------- \n Opcion Invalida !! \n---------");
+				view.printObject("--------- \n Opcion Invalida !! \n---------");
 				break;
 			}
 		}
